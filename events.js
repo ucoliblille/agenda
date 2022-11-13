1477c84934e68bd7c69b942ef0c1b7c961d8e1a5
ucoliblille <74920682+ucoliblille@users.noreply.github.com>
2022-11-13 17:09:17 +0100

    Add files via upload

---
+++ events.js
@@ -0,0 +1,16 @@
+const data = [
+    {
+        nom: "Réunion",
+        qui: "UCL",
+        description: "Réunion bi-mensuelle",
+        lieu: 'CCL',
+        annee: 2022,
+        mois: 11,
+        jourDebut: 17,
+        jourFin: 17,
+        heureDebut: 18,
+        minuteDebut: 30,
+        heureFin: 20,
+        minuteFin: 30
+    }
+]
